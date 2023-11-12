1e7bd757ae42585953a05b84e06f01c0aafcd938
Annie <at.anna.thorne@gmail.com>
2023-11-12 12:30:50 +0100

    Add deletion confirmation dialog strings to resources

--- app/src/main/java/com/thorne/pillapp/MainActivity.kt
+++ app/src/main/java/com/thorne/pillapp/MainActivity.kt
@@ -19,13 +19,17 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.DeleteForever
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -48,8 +52,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.thorne.pillapp.ui.dialogues.ConfirmDialog
 import com.thorne.pillapp.ui.medicine.create.CreateMedicineActivity
 import com.thorne.pillapp.ui.theme.PillAppTheme
 import com.thorne.sdk.MedSdkImpl
@@ -134,7 +140,9 @@ class MainActivity : ComponentActivity() {
 
     //TODO Add a edit button and stuff
     @Composable
-    private fun CardContent(med: Medication, onDeleteClick: (Medication) -> Unit, context: Context) {
+    private fun CardContent(
+        med: Medication, onDeleteClick: (Medication) -> Unit, context: Context
+    ) {
         var expanded by remember { mutableStateOf(false) }
 
         Row(
@@ -152,50 +160,135 @@ class MainActivity : ComponentActivity() {
                     .weight(1f)
                     .padding(12.dp)
             ) {
-                Text(
-                    text = SimpleDateFormat(
-                        "dd/MM/yy", Locale.getDefault()
-                    ).format(Date(med.getStartDate())) + " - " + SimpleDateFormat(
-                        "dd/MM/yy", Locale.getDefault()
-                    ).format(Date(med.getEndDate()))
-                )
-                // Text(text = med.getHourlyFrequency().toString() + " times a day")
-                Text(
-                    text = med.getName(), style = MaterialTheme.typography.headlineMedium.copy(
-                        fontWeight = FontWeight.ExtraBold
-                    )
-                )
-                if (expanded) {
+                Row {
                     Text(
-                        text = med.getNotes()
+                        text = SimpleDateFormat(
+                            "dd/MM/yy", Locale.getDefault()
+                        ).format(Date(med.getStartDate())) + " - " + SimpleDateFormat(
+                            "dd/MM/yy", Locale.getDefault()
+                        ).format(Date(med.getEndDate())),
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            fontWeight = FontWeight.Bold
+                        ),
+                        textAlign = TextAlign.Center
                     )
                 }
-            }
-            IconButton(onClick = { expanded = !expanded }) {
-                Icon(
-                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                    contentDescription = if (expanded) {
-                        stringResource(R.string.show_less)
-                    } else {
-                        stringResource(R.string.show_more)
+
+                Row {
+                    Text(
+                        text = med.getName(), style = MaterialTheme.typography.headlineMedium.copy(
+                            fontWeight = FontWeight.ExtraBold
+                        )
+                    )
+                }
+                Row {
+                    Text(
+                        text = med.getDosage(), style = MaterialTheme.typography.headlineSmall.copy(
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+
+                if (expanded) {
+                    Row {
+                        Text(
+                            text = getString(R.string.medicine_frequency) + ":",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.Normal
+                            ),
+                            textAlign = TextAlign.Center
+                        )
+
+                        Text(
+                            text = med.getFrequency().toString(),
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.Bold
+                            ),
+                            textAlign = TextAlign.Center
+                        )
+
                     }
-                )
+
+                    Row {
+                        Text(
+                            text = getString(R.string.medicine_notes) + ":",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.Normal
+                            ),
+                            textAlign = TextAlign.Center
+                        )
+
+                        Text(
+                            text = med.getNotes(), style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.Bold
+                            ), textAlign = TextAlign.Center
+                        )
+
+                    }
+                }
             }
-//            delete button
-            IconButton(onClick = {
-                onDeleteClick(med)
-                Toast.makeText(
-                    context,
-                    context.getString(R.string.success),
-                    Toast.LENGTH_LONG
-                ).show()
-            }) {
-                Icon(
-                    imageVector = Icons.Filled.Delete,
-                    contentDescription = "Delete"
-                )
+            Column (
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                // Expand button
+                Row {
+                    IconButton(onClick = { expanded = !expanded }) {
+                        Icon(
+                            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                            contentDescription = if (expanded) {
+                                stringResource(R.string.show_less)
+                            } else {
+                                stringResource(R.string.show_more)
+                            }
+                        )
+                    }
+                }
+                if (expanded) {
+                    // Edit button
+                    Row {
+                        IconButton(onClick = {
+
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Edit, contentDescription = "Edit"
+                            )
+                        }
+                    }
+
+                    // Delete button
+                    Row {
+                        val openConfirmDialog = remember { mutableStateOf(false) }
+
+                        val cd = ConfirmDialog()
+                        if (openConfirmDialog.value) {
+                            cd.ShowConfirmationDialog(
+                                onDismissRequest = { openConfirmDialog.value = false },
+                                onConfirmation = {
+                                    onDeleteClick(med)
+                                    openConfirmDialog.value = false
+                                    Toast.makeText(
+                                        context,
+                                        context.getString(R.string.success),
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                },
+                                dialogTitle = stringResource(R.string.dialog_deletion_title),
+                                dialogText = stringResource(R.string.dialog_deletion_text),
+                                icon = Icons.Default.DeleteForever
+                            )
+                        }
+                        IconButton(onClick = {
+                            openConfirmDialog.value = true
+
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Delete, contentDescription = "Delete"
+                            )
+                        }
+                    }
+                }
             }
-    }
+        }
     }
 
 
@@ -260,8 +353,13 @@ class MainActivity : ComponentActivity() {
                             stiffness = Spring.StiffnessLow
                         )
                     )
+                    .size(62.dp)
             ) {
-                Icon(Icons.Filled.Add, "Floating action button.")
+                Icon(
+                    Icons.Filled.Add,
+                    "Add medicine button",
+                    modifier = Modifier.size(36.dp)
+                )
             }
         }
     }
